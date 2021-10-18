30d7a125856a4abe251fd441226cd467314775e5
Jean Raby <jean@raby.sh>
2021-10-18 18:34:04 -0400

    Support multiple prefix->root mappings
    Only reply to requests ending in .zip
    Disallow top .zip request  (e.g /prefix/.zip)
    Use Echo's router to route requests using prefix + "/*"

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"archive/zip"
-	"flag"
 	"fmt"
 	"io"
 	"net/http"
@@ -16,87 +15,107 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	flag "github.com/spf13/pflag"
 )
 
-type DirZip struct {
-	Prefix string
-	Root   string
-}
+// TODO: support subdirs?
 
-func (dz *DirZip) handler(c echo.Context) error {
-	r := c.Request()
-	urlpath := path.Clean(r.URL.Path)
-	realreqpath := dz.Root + "/" + strings.TrimPrefix(urlpath, dz.Prefix)
-	stat, err := os.Stat(realreqpath)
-	if err != nil || !stat.IsDir() {
-		c.NoContent(http.StatusNotFound)
+func notfound(c echo.Context) error {
+	c.String(http.StatusNotFound, "Not Found")
+	return nil
+}
+func makehandler(prefix, root string) func(echo.Context) error {
+
+	handler := func(c echo.Context) error {
+		r := c.Request()
+		urlpath := path.Clean(r.URL.Path)
+
+		if !strings.HasSuffix(urlpath, ".zip") {
+			return notfound(c)
+		}
+
+		urlpath = strings.TrimSuffix(urlpath, ".zip")
+
+		urlpathnoprefix := strings.TrimPrefix(urlpath, prefix)
+		log.Printf("urlpathnoprefix: %s\n", urlpathnoprefix)
+		if len(urlpathnoprefix) == 0 || urlpathnoprefix == "/" {
+			return notfound(c)
+		}
+		realreqpath := root + "/" + urlpathnoprefix
+
+		stat, err := os.Stat(realreqpath)
+		if err != nil || !stat.IsDir() {
+			return notfound(c)
+		}
+
+		filename := path.Base(urlpath + ".zip")
+		c.Response().Header().Set("Content-Type", "application/zip")
+		c.Response().Header().Set("Content-Disposition", `attachment; filename="`+filename+`"`)
+		c.Response().WriteHeader(http.StatusOK)
+
+		zw := zip.NewWriter(c.Response())
+		defer zw.Close()
+
+		// Walk directory.
+		filepath.Walk(realreqpath, func(p string, info os.FileInfo, err error) error {
+			if info.IsDir() {
+				// XXX add support for subdirectories,  also handle symlinks
+				return nil
+			}
+
+			fmt.Printf("path: %s\n", p)
+
+			// XXX with subdir support this will have to change
+			ze, err := zw.CreateHeader(&zip.FileHeader{Name: path.Base(p), Method: 0, Modified: info.ModTime()})
+			if err != nil {
+				return fmt.Errorf("Failed for %p: %s", err)
+			}
+			file, err := os.Open(p)
+			if err != nil {
+				return fmt.Errorf("Cannot open file %s: %s\n", p, err)
+			}
+			defer file.Close()
+
+			_, err = io.Copy(ze, file)
+			return err
+		})
+
 		return nil
 	}
 
-	filename := path.Base(urlpath + ".zip")
-	c.Response().Header().Set("Content-Type", "application/zip")
-	c.Response().Header().Set("Content-Disposition", `attachment; filename="`+filename+`"`)
-	c.Response().WriteHeader(http.StatusOK)
-
-	zw := zip.NewWriter(c.Response())
-	defer zw.Close()
-	// Walk directory.
-	filepath.Walk(realreqpath, func(p string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			// XXX add support for subdirectories,  also handle symlinks
-			return nil
-		}
-
-		fmt.Printf("path: %s\n", p)
-
-		// XXX with subdir support this will have to change
-		ze, err := zw.CreateHeader(&zip.FileHeader{Name: path.Base(p), Method: 0, Modified: info.ModTime()})
-		if err != nil {
-			return fmt.Errorf("Failed for %p: %s", err)
-		}
-		file, err := os.Open(p)
-		if err != nil {
-			return fmt.Errorf("Cannot open file %s: %s\n", p, err)
-		}
-		defer file.Close()
-
-		_, err = io.Copy(ze, file)
-		return err
-	})
-
-	return nil
+	return handler
 }
 
 func main() {
-	var allowedPrefix, docRoot, listen string
-	var port int
-	flag.StringVar(&allowedPrefix, "allowedPrefix", "", "URL prefix for which request will be allowed")
-	flag.StringVar(&docRoot, "docRoot", "", "Document root. Prefix is stripped from this and the rest is looked up under here")
-	flag.StringVar(&listen, "listen", "127.0.0.1", "Listen address")
-	flag.IntVar(&port, "port", 10666, "Listen port")
+	prefix2root := flag.StringToString("allow", nil, "Add an allowed url prefix->docroot mapping. e.g /media/patate:/path/to/media/patate")
+	listen := flag.String("listen", "127.0.0.1", "Listen address")
+	port := flag.Int("port", 10666, "Listen port")
 
 	flag.Parse()
 
-	if docRoot == "" || allowedPrefix == "" {
-		flag.CommandLine.Usage()
-		log.Fatal("Missing docRoot or allowedPrefix")
+	if len(*prefix2root) == 0 {
+		flag.Usage()
+		log.Fatal("No configuration. Set --allow")
 	}
 
-	if port < 1 {
-		flag.CommandLine.Usage()
-		log.Fatalf("Invalid port: %d\n", port)
+	if *port < 1 {
+		flag.Usage()
+		log.Fatalf("Invalid port: %d\n", *port)
 	}
 
-	dz := DirZip{Prefix: allowedPrefix, Root: docRoot}
-
-	log.Printf("Root: %s\nListening On: %s:%d\n", docRoot, listen, port)
+	log.Printf("Listening On: %s:%d\n", *listen, *port)
 
 	e := echo.New()
 	e.HideBanner = true
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET(path.Clean(allowedPrefix+"/*"), dz.handler)
-	e.Logger.Fatal(e.Start(listen + ":" + strconv.Itoa(port)))
+	for prefix, root := range *prefix2root {
+		cp := path.Clean(prefix)
+		cpany := cp + "/*"
+		log.Printf("Adding allowed prefix: %s -> %s\n", cpany, root)
+		e.GET(cpany, makehandler(cp, root))
+	}
+	e.Logger.Fatal(e.Start(*listen + ":" + strconv.Itoa(*port)))
 
 }
